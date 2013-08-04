ba57275e422f43d7580298f5a0f521c12f17aba1
SpaceShawn <shawn.bulger@spaceconcordia.ca>
2013-08-04 17:54:26 -0400

    Command-line prototype and lib

---
+++ transceiver.py
@@ -0,0 +1,83 @@
+from array import *
+import time
+import serial
+from transclib import *
+
+ser = serial.Serial(
+    3,
+  baudrate=9600,
+  parity=serial.PARITY_NONE,
+  bytesize=serial.EIGHTBITS,
+  stopbits=serial.STOPBITS_ONE,
+  timeout=1,
+  writeTimeout = 3
+  )
+
+#  print "error opening serial port: " + str(e)
+#  exit()
+
+if ser.isOpen():
+  print 'Enter commands for the transceiver below.\r\nType "menu" for predefined commands, and "exit" to quit'
+  input=1
+  while 1:
+    input=raw_input(">> ")
+    if input == "exit":
+      ser.close()
+      exit()
+    elif input == "menu":
+      print 'noop - send no-op sequence\r'
+      print 'getconfig - send getConfig\r'
+      print 'setconfig - send setconfig\r'
+      print 'transmit - transmit hard-coded data\r\n'
+
+    elif input == "noop":
+      input = SC_noop()
+      #ser.write(input + '\r\n')
+      ser.write(input)
+      out = ''	
+      time.sleep(1);
+
+      while ser.inWaiting() > 0:
+	out += ser.read(1)
+
+      if out!= '':
+	print toHex(out)
+	print ('>>')
+    elif input == "transmit":
+      print 'Enter a message to transmit'
+      input=raw_input()
+
+      ser.write(SC_transmit(input))
+      print 'Response'
+
+      out = ''	
+      time.sleep(1);
+
+      while ser.inWaiting() > 0:
+		out += ser.read(1)
+
+      if out!= '':
+		print toHex(out)
+		print ('>>')
+    elif input == "checksum":
+      print 'Enter a message to checksum'
+      input=raw_input()
+      print SC_fletcher8(input)
+#      print SC_fletcher16(input)
+      print SC_fletcher32(input)
+    else:
+      ser.write(input + '\r\n')
+      out = ''	
+      time.sleep(1);
+
+      while ser.inWaiting() > 0:
+	out += ser.read(1)
+
+	if out!= '':
+	  print ">>" + out
+#	except Exception, e1:
+#		print "error communicating...:" + str(e1)
+else :
+  ser.open()
+	#except Exception, e:
+	#print "Cannot open serial port"
---
+++ transclib.py
@@ -0,0 +1,112 @@
+from math import *
+import struct
+import sys
+
+def SC_computeFletcher(data, size, modulo, limit=None):
+	valA, valB = 1, 0
+	length = len(data)	
+	if isinstance(data, str):
+		if limit is not None and length > limit:
+			data = data[:limit]
+		for char in data:
+			valA += ord(char)
+			valB += valA
+			valA %= modulo
+			valB %= modulo
+	else:
+		if limit is not None and length > limit:
+			data = data[:limit]
+		for c in data:
+			valA += c
+			valB += valA
+			valA %= modulo
+			valB %= modulo
+#	return (valA << (size/2), valB)
+	return (valA, valB)
+#	return (valB << (size/2)) + valA
+
+def SC_fletcher8(data):
+  return SC_computeFletcher(data, 8, 255, limit=21)
+
+def SC_fletcher32(data):
+  return SC_computeFletcher(data, 32, 65535, limit=360)
+
+def SC_fletcher(data):
+  return SC_fletcher8(data)
+
+def SC_noop(): 
+  return bytearray.fromhex('48 65 10 01 00 00 11 43 00 00')
+
+def SC_transmit(msg): 
+	length = unsigned(int(len(msg)))  % 2**8
+	#print "var length: " + type(length)
+	lengthbytes = struct.pack('B', length)
+	print 'payload leng: ' + str(length)
+	msg_checksum = SC_fletcher(msg)
+	print 'messagecheck: ' + str(msg_checksum)
+	
+	# header
+	header = '48 65 10 03' #H E transmit ?? 1st_length
+	packet = bytearray.fromhex(header)
+	print 'header base : ' + toHex(str(packet))
+
+	# length
+	# add '00' byte before length
+	packet.extend(bytearray.fromhex('00'))
+	packet.extend(lengthbytes)
+	print 'added length: ' + toHex(str(packet))
+	print 'length.size : ' + SC_size(lengthbytes) + ' bytes'
+	header_checksum = SC_fletcher(packet)
+	print 'headck1 size: ' + SC_size(header_checksum[0]) + " bytes";
+	print 'headck2 size: ' + SC_size(header_checksum[1]) + " bytes";
+	print 'header check: ' + str(header_checksum)
+
+	packet.extend(struct.pack('B', header_checksum[0]))
+	packet.extend(struct.pack('B', header_checksum[1]))
+#	packet.extend(intToBytes(header_checksum[0],1))
+#	packet.extend(intToBytes(header_checksum[1],1))
+		
+	print 'full header : ' + toHex(str(packet))
+	
+	packet.extend(msg.encode('utf-8'))
+
+	print 'fullhead+msg: ' + toHex(str(packet))
+	print 'msg checksum: ' + toHex(str(msg_checksum[0])) + toHex(str(msg_checksum[1]));
+	print 'msgchk1 size: ' + SC_size(msg_checksum[0]) + " bytes";
+	print 'msgchk2 size: ' + SC_size(msg_checksum[1]) + " bytes";
+	
+	packet.extend(struct.pack('B', msg_checksum[0]))
+	packet.extend(struct.pack('B', msg_checksum[1]))
+#	packet.extend(intToBytes(msg_checksum[0],1))
+#	packet.extend(intToBytes(msg_checksum[1],1))
+
+	print 'fullmsginhex: ' + toHex(str(packet))
+
+	return packet
+
+def SC_getConfig():
+  return ''
+
+def SC_setConfig():
+  return false;
+
+def SC_size(data):
+	return str(sys.getsizeof(data))
+
+def unsigned(n):
+	return n & 0xFFFFFFFF
+
+def toHex(s):
+  lst = []
+  for ch in s:
+    hv = hex(ord(ch)).replace('0x', '')
+    if len(hv) == 1:
+      hv = '0'+hv	   
+    lst.append(hv)
+  return reduce(lambda x,y:x+y, lst)
+
+def toStr(s):
+  return s and chr(atoi(s[:2], base=16)) + toStr(s[2:]) or ''
+
+def intToBytes(val, num_bytes):
+	return [(val & (0xff << pos*8)) >> pos*8 for pos in range(num_bytes)]
