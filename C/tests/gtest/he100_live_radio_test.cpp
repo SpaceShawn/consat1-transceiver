20110c53772562348331343876fd8678dc2bb0e0
Shawn B <shawn.bulger@spaceconcordia.ca>
2016-05-10 21:01:42 -0400

    Making editing the config buffer more user friendly

--- C/tests/gtest/he100_live_radio_test.cpp
+++ C/tests/gtest/he100_live_radio_test.cpp
@@ -48,7 +48,49 @@ unsigned char * HE100_prepareTransmission (unsigned char *payload, size_t length
 
 TEST_F(Helium_100_Live_Radio_Test, SetConfig)
 {
-    unsigned char config[CFG_PAYLOAD_LENGTH] = {0x00,0x00,0x01,0x01,0x00,0x00,0x48,0x33,0x02,0x00,0x98,0x93,0x06,0x00,0x56,0x41,0x33,0x4f,0x52,0x42,0x56,0x45,0x32,0x43,0x55,0x41,0x05,0x00,0x00,0x00,0x41,0x80,0x00,0x00};
+    unsigned char config0[CFG_PAYLOAD_LENGTH] = {0x00,0x00,0x01,0x01,0x00,0x00,0x48,0x33,0x02,0x00,0x98,0x93,0x06,0x00,0x56,0x41,0x33,0x4f,0x52,0x42,0x56,0x45,0x32,0x43,0x55,0x41,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+    unsigned char config[CFG_PAYLOAD_LENGTH];
+    config[CFG_IF_BAUD_BYTE] = CFG_IF_BAUD_9600;
+    config[CFG_PA_BYTE] = 0;
+    config[CFG_RF_RX_BAUD_BYTE] = CFG_RF_BAUD_9600;
+    config[CFG_RF_TX_BAUD_BYTE] = CFG_RF_BAUD_9600;
+    config[CFG_RX_MOD_BYTE] = CFG_RX_MOD_GFSK;
+    config[CFG_TX_MOD_BYTE] = CFG_TX_MOD_GFSK;
+    //config[CFG_RX_FREQ_BYTE1] = CFG_RX_FREQ_DEFAULT; // 144200L
+    config[CFG_RX_FREQ_BYTE1] = 0x48;
+    config[CFG_RX_FREQ_BYTE2] = 0x33;
+    config[CFG_RX_FREQ_BYTE3] = 0x02;
+    config[CFG_RX_FREQ_BYTE4] = 0x00;
+    //config[CFG_TX_FREQ_BYTE1] = CFG_TX_FREQ_DEFAULT; // 431000L
+    config[CFG_TX_FREQ_BYTE1] = 0x98;
+    config[CFG_TX_FREQ_BYTE2] = 0x93;
+    config[CFG_TX_FREQ_BYTE3] = 0x06;
+    config[CFG_TX_FREQ_BYTE4] = 0x00;
+
+    char source_callsign[7] = "VA3ORB";
+    char destination_callsign[7] = "VE2CUA";
+    //config[CFG_SRC_CALL_BYTE] = CFG_SRC_CALL_DEF; // "VA3ORB"
+    memcpy(config+CFG_SRC_CALL_BYTE, &source_callsign,7);
+    //config[CFG_DST_CALL_BYTE] = CFG_DST_CALL_DEF; // "VE2CUA"
+    memcpy(config+CFG_DST_CALL_BYTE, &destination_callsign,7);
+
+    config[CFG_TX_PREAM_BYTE] = CFG_TX_PREAM_DEF;
+    config[CFG_TX_PREAM_BYTE] = CFG_TX_PREAM_DEF;
+    config[CFG_TX_POSTAM_BYTE] = CFG_TX_POSTAM_DEF;
+    config[CFG_FUNCTION_CONFIG_BYTE] = 0;
+    config[CFG_FUNCTION_CONFIG_BYTE+1] = 0;
+    config[CFG_FUNCTION_CONFIG2_BYTE] = 0;
+    config[CFG_FUNCTION_CONFIG2_BYTE+1] = 0;
+
+    printf ("\nByte: x \t CFG_BYTE_LIST             \t Exp\t :\t Act\n");
+    for (z=0; z<CFG_PAYLOAD_LENGTH; z++) {
+        printf ("Byte: %d \t %s \t 0x%X\t :\t 0x%X\n",z,CFG_BYTE_LIST[z],config0[z],config[z]);
+        ASSERT_EQ(
+            config0[z],
+            config[z]
+        );
+    }
+
     struct he100_settings settings = HE100_collectConfig(config);
 
     FILE *test_log;
